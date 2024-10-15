9cf32f33a29d2349274900891d3f6a6bc35e705b
patmorgan1302 <patrickmorgan1302@gmail.com>
2024-10-14 23:45:13 -0600

    working

--- frontend/src/Screens/HomeScreen.jsx
+++ frontend/src/Screens/HomeScreen.jsx
@@ -17,21 +17,37 @@ const HomeScreen = () => {
             ) : error ? ( 
                 <Message variant='danger'>{ error?.data?.message || error.error} </Message>
             ) : (
-           <>
-            <div>
-                <h1>Latest Products</h1>
+            <>
                 <Row>
-                    {products.map((product) => (
-                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                            <Product product={product} />
-                        </Col>
-                    ))}
+                    <Col>
+                        <div>
+                            <h1>Latest Products</h1>
+                            <Row>
+                                {products.map((product) => (
+                                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                        <Product product={product} />
+                                    </Col>
+                                ))}
+                            </Row>
+                        </div>
+                    </Col>
+
+                    <Col>
+                    <div>
+                            <h1>Latest Products</h1>
+                            <Row>
+                                {products.map((product) => (
+                                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                        <Product product={product} />
+                                    </Col>
+                                ))}
+                            </Row>
+                        </div>
+                    </Col>
                 </Row>
-            </div>
             </>
             )}
-           
-       </>
+          </>
     )
 };
 
